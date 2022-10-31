f126a19fe846d54b1c710b9598e8ab2ff7901a49
Maciej Puchalski <maciejpe34@gmail.com>
2022-10-31 11:22:31 +0100

    Created Shopping List app with windows forms

--- ShoppingList/Form1.Designer.cs
+++ ShoppingList/Form1.Designer.cs
@@ -28,13 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.btnUsun = new System.Windows.Forms.Button();
+            this.btnCzysc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 28;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(233, 368);
+            this.listBox1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(233, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(12, 415);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDodaj.Location = new System.Drawing.Point(31, 452);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(194, 36);
+            this.btnDodaj.TabIndex = 3;
+            this.btnDodaj.Text = "Add";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            this.btnDodaj.Click += new System.EventHandler(this.btnDodaj_Click);
+            // 
+            // btnUsun
+            // 
+            this.btnUsun.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnUsun.Location = new System.Drawing.Point(31, 494);
+            this.btnUsun.Name = "btnUsun";
+            this.btnUsun.Size = new System.Drawing.Size(89, 36);
+            this.btnUsun.TabIndex = 4;
+            this.btnUsun.Text = "Delete";
+            this.btnUsun.UseVisualStyleBackColor = true;
+            this.btnUsun.Click += new System.EventHandler(this.btnUsun_Click);
+            // 
+            // btnCzysc
+            // 
+            this.btnCzysc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnCzysc.Location = new System.Drawing.Point(136, 494);
+            this.btnCzysc.Name = "btnCzysc";
+            this.btnCzysc.Size = new System.Drawing.Size(89, 36);
+            this.btnCzysc.TabIndex = 5;
+            this.btnCzysc.Text = "Clear";
+            this.btnCzysc.UseVisualStyleBackColor = true;
+            this.btnCzysc.Click += new System.EventHandler(this.btnCzysc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(264, 541);
+            this.Controls.Add(this.btnCzysc);
+            this.Controls.Add(this.btnUsun);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "My Shopping List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Button btnUsun;
+        private System.Windows.Forms.Button btnCzysc;
     }
 }
 
--- ShoppingList/Form1.cs
+++ ShoppingList/Form1.cs
@@ -16,5 +16,62 @@ namespace ShoppingList
         {
             InitializeComponent();
         }
+
+    
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            if(progressBar1.Value <100)
+            {
+                if(textBox1.Text.Length>0)
+                {
+                    if(listBox1.Items.Contains(textBox1.Text))
+                    {
+                        DialogResult resault = MessageBox.Show("Element already exists, Do you want to add it?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                        if(resault == DialogResult.No)
+                        {
+                            return;
+                        }
+                    }
+                    listBox1.Items.Add(textBox1.Text);
+                    textBox1.Clear();
+                    AktualizujProgres();
+                }
+                else
+                {
+                     MessageBox.Show("Not item found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("List is full. Delete something to add something else!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void AktualizujProgres()
+        {
+            int iloscelementow = listBox1.Items.Count;
+            progressBar1.Value = iloscelementow * 10;
+        }
+
+        private void btnUsun_Click(object sender, EventArgs e)
+        {
+            int indexOfElement = listBox1.SelectedIndex;
+            if(indexOfElement != -1)
+            {
+                listBox1.Items.RemoveAt(indexOfElement);
+                AktualizujProgres();
+            }
+            else
+            {
+                MessageBox.Show("None element have been selected!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCzysc_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            AktualizujProgres();
+        }
     }
 }
